8642a24dc45871ef8c760e3de3a812bb6f3ebdd2
z2s <zen2see@gmail.com>
2019-10-09 23:14:51 -0400

    Fixed ownable.sol lack of payable address with new OpenZepplin .sol file

---
+++ contracts/ZombieOwnership.sol
@@ -0,0 +1,55 @@
+pragma solidity >=0.5.0;
+
+import "./zombieattack.sol";
+import "./erc721.sol";
+import "./safemath.sol";
+
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+  mapping (uint => address) zombieApprovals;
+
+  function balanceOF(address _owner)
+    external
+    view
+    returns (uint256)
+  {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId)
+    external
+    view
+    returns (address)
+  {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
+    ownerZombieCount[_from] = ownerZombieCount[_from].sub(1);
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId)
+    external
+    payable
+  {
+    require(
+      zombieToOwner[_tokenId] == msg.sender ||
+      zombieApprovals[_tokenId] == msg.sender,
+      "only approved address (caller) can tranfer"
+    );
+    _transfer(_from, _to, _tokenId);
+  }
+
+  function approve(address _approved, uint256 _tokenId)
+    external
+    payable
+    onlyOwnerOf(_tokenId)
+  {
+    zombieApprovals[_tokenId] = _approved;
+    emit Approval(msg.sender, _approved, _tokenId);
+  }
+
+}
